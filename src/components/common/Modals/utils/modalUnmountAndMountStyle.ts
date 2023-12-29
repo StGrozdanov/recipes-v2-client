f2366b88b79e15dcd81cdea33c15ac642d028f13
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2023-12-29 16:14:08 +0200

    fix login form link bug

--- src/components/common/Modals/utils/modalUnmountAndMountStyle.ts
+++ src/components/common/Modals/utils/modalUnmountAndMountStyle.ts
@@ -1,11 +1,13 @@
 export const unmountedStyleModal = {
     opacity: 0,
     top: '100vh',
-    transition: 'all 700ms ease-in'
+    transition: 'all 700ms ease-in',
+    zIndex: 0,
 };
 
 export const mountedStyleModal = {
     opacity: 1,
     bottom: '100vh',
-    transition: 'all 400ms ease-in'
+    transition: 'all 400ms ease-in',
+    zIndex: 0,
 };
