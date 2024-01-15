493d25f212fcf655fc524bc9b1e433c8b2c0742a
Stoyan Grozdanov <stoyan@Stoyans-MacBook-Pro.local>
2024-01-15 06:45:55 +0200

    improve search responsive design

--- src/components/Navigation/modules/Search/Search.tsx
+++ src/components/Navigation/modules/Search/Search.tsx
@@ -3,6 +3,7 @@ import styles from './Search.module.scss';
 import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { useMediaQuery } from 'react-responsive'
 
 type SearchProps = {
     show: boolean,
@@ -10,12 +11,16 @@ type SearchProps = {
 }
 
 const unmountedStyle = { left: '50vw', bottom: '100%', transition: 'all .6s ease-in' };
-const mountedStyle = {top: '74px', transition: 'all .6s ease-out' };
+const mobileMountedStyle = { top: '70px', transition: 'all .6s ease-out' };
+const mountedStyle = { top: '85px', transition: 'all .6s ease-out' };
+const bigScreenMountedStyle = { top: '140px', transition: 'all .6s ease-out' };
 const inputUnmountedStyle = { background: 'white', transition: 'all 0.7s ease-in' };
 
 export default function Search({ show, hideSearchHandler }: SearchProps) {
     const [searchInputValue, setSearchInputValue] = useState('');
     const navigate = useNavigate();
+    const isTabletOrMobile = useMediaQuery({ query: '(max-width: 1224px)' });
+    const isBigScreen = useMediaQuery({ query: '(min-width: 1824px)' });
 
     const searchHandler = (e: React.MouseEvent<SVGSVGElement, MouseEvent> | React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -30,7 +35,15 @@ export default function Search({ show, hideSearchHandler }: SearchProps) {
     return (
         <form
             className={styles['search-article']}
-            style={!show ? unmountedStyle : mountedStyle}
+            style={
+                !show
+                    ? unmountedStyle
+                    : isTabletOrMobile
+                        ? mobileMountedStyle
+                        : isBigScreen
+                            ? bigScreenMountedStyle
+                            : mountedStyle
+            }
             onSubmit={searchHandler}
         >
             <FontAwesomeIcon
